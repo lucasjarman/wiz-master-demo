276b0656d54d822b868dbc0399dc5dfbe6974427
lucasjarman <lucas.jarman@wiz.io>
2026-02-07 21:22:57 +1100

    fix: use fully defined backend.tf for Wiz Code-to-Cloud mapping
    - Modified init-backends task to generate backend.tf with bucket/key/region hardcoded
    - Updated infrastructure/demo/backend.tf to be fully defined
    - Wiz requires fully defined backends (iac_config.wiz doesn't work reliably)
    - Bucket name updates automatically when running init-backends after bootstrap

--- infrastructure/demo/backend.tf
+++ infrastructure/demo/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
+    bucket       = "demo-dev-c1dfca-state-bucket-ap-southeast-2"
+    key          = "infrastructure/demo/terraform.tfstate"
+    region       = "ap-southeast-2"
     encrypt      = true
     use_lockfile = true
-    region       = "ap-southeast-2"
   }
 }
